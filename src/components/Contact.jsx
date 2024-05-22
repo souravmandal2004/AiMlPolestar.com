2d15489c6f4cc387625b4bc5296c58f950fee98a
souravmandal2004 <mandalsourav2004@gmail.com>
2024-05-22 21:28:42 +0530

    aimlpolestar.com

---
+++ src/components/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { MdOutlineLocationOn } from "react-icons/md";
+import { MdEmail } from "react-icons/md";
+
+function Contact() {
+    return (
+        <div name='contact' className='bg-[#0a192f] w-full overflow-x-hidden h-screen'>
+            <div className='max-w-[1600px] p-3 mx-auto mt-36 grid grid-cols-1 sm:grid-cols-2 gap-6'>
+                {/* address */}
+                <div className='text-white  max-w-[800px] bg-[#214392] flex flex-col justify-center items-center p-[2em] rounded-lg gap-6 hover:bg-[#0A192F] transition-all duration-300'>
+                    <MdOutlineLocationOn size={50} color='#B7094C'/>
+                    <h3 className='font-bold text-2xl'>Our Branches</h3>
+                    <p className='text-xl font-semibold'> Plot no. A23, Raghunathpur, Bhubaneswar, Odisha, 751024</p>
+                </div>
+
+                {/* email  */}
+                <div className='text-white bg-[#214392] max-w-[800px] flex flex-col justify-center items-center p-[2em] rounded-lg gap-6 hover:bg-[#0A192F] transition-all duration-300'>
+                    <MdEmail size={50} color='#B7094C'/>
+                    <h3 className='text-2xl font-bold'>Email</h3>
+                    <p className='font-semibold text-xl text-center'>contact@aimlpolestar.com</p>
+                </div>
+            </div>
+
+        </div>
+    );
+}
+export default Contact;
